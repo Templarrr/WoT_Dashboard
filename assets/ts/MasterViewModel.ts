a28763d1334d8471ce2fcb2331626b417e06fb43
Michael Odintsov <templarrrr@gmail.com>
2014-07-18 17:51:35 +0300

    Filtering by level|country|type

--- assets/ts/MasterViewModel.ts
+++ assets/ts/MasterViewModel.ts
@@ -10,6 +10,13 @@ module ViewModels {
         navVM:NavigationViewModel = new ViewModels.NavigationViewModel();
         tanksList:KnockoutObservableArray<any> = ko.observableArray([]);
 
+        public createSelect(aData:string[], caption = 'All'):string {
+            var selStart = '<select><option value="" selected="selected">' + caption + '</option>', i, iLen = aData.length;
+            var selMiddle = _.reduce(aData, (memo, data) => memo + '<option value="' + data + '">' + data + '</option>', '');
+            var selEnd = '</select>';
+            return selStart + selMiddle + selEnd;
+        }
+
         public getTanksInfo():void {
             var self = this;
             var promise = $.ajax({
@@ -23,7 +30,31 @@ module ViewModels {
                     var levelStr = tank.level < 10 ? '0' + tank.level : '' + tank.level;
                     return tank.nation_i18n + '_' + levelStr + '_' + tank.name_i18n;
                 }));
-                $('#tanksTable').dataTable({});
+                var tanksTable = $('#tanksTable').dataTable({
+                    "sDom": "<'row'<'col-lg-4'l><'col-lg-4'f><'col-lg-4 customFilters'c>r>t<'row'<'col-lg-12'i><'col-lg-12 center'p>>"
+                });
+                var allSelects = self.createSelect(_.unique(_.pluck(self.tanksList(), 'nation_i18n')), 'Все страны') + ' ' +
+                    self.createSelect(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'], 'Все уровни') + ' ' +
+                    self.createSelect(_.unique(_.pluck(self.tanksList(), 'type_i18n')), 'Все типы');
+                $('.customFilters').html(allSelects);
+                $('.customFilters select:nth-child(1)').change(function () {
+                    tanksTable.fnFilter($(this).val(), 1);
+                });
+                $('.customFilters select:nth-child(2)').change(function () {
+                    if ($(this).val() == '') {
+                        tanksTable.fnFilter('', 0);
+                    } else {
+                        tanksTable.fnFilter("^" + $(this).val() + "$", 0, true, false);
+                    }
+                });
+                $('.customFilters select:nth-child(3)').change(function () {
+                    if ($(this).val() == '') {
+                        tanksTable.fnFilter('', 3);
+                    } else {
+                        tanksTable.fnFilter("^" + $(this).val() + "$", 3, true, false);
+                    }
+                });
+
             })
         }
 
--- assets/ts/index.js
+++ assets/ts/index.js
@@ -36,6 +36,16 @@ var ViewModels;
             this.tanksList = ko.observableArray([]);
             this.getTanksInfo();
         }
+        MasterViewModel.prototype.createSelect = function (aData, caption) {
+            if (typeof caption === "undefined") { caption = 'All'; }
+            var selStart = '<select><option value="" selected="selected">' + caption + '</option>', i, iLen = aData.length;
+            var selMiddle = _.reduce(aData, function (memo, data) {
+                return memo + '<option value="' + data + '">' + data + '</option>';
+            }, '');
+            var selEnd = '</select>';
+            return selStart + selMiddle + selEnd;
+        };
+
         MasterViewModel.prototype.getTanksInfo = function () {
             var self = this;
             var promise = $.ajax({
@@ -49,7 +59,28 @@ var ViewModels;
                     var levelStr = tank.level < 10 ? '0' + tank.level : '' + tank.level;
                     return tank.nation_i18n + '_' + levelStr + '_' + tank.name_i18n;
                 }));
-                $('#tanksTable').dataTable({});
+                var tanksTable = $('#tanksTable').dataTable({
+                    "sDom": "<'row'<'col-lg-4'l><'col-lg-4'f><'col-lg-4 customFilters'c>r>t<'row'<'col-lg-12'i><'col-lg-12 center'p>>"
+                });
+                var allSelects = self.createSelect(_.unique(_.pluck(self.tanksList(), 'nation_i18n')), 'Все страны') + ' ' + self.createSelect(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'], 'Все уровни') + ' ' + self.createSelect(_.unique(_.pluck(self.tanksList(), 'type_i18n')), 'Все типы');
+                $('.customFilters').html(allSelects);
+                $('.customFilters select:nth-child(1)').change(function () {
+                    tanksTable.fnFilter($(this).val(), 1);
+                });
+                $('.customFilters select:nth-child(2)').change(function () {
+                    if ($(this).val() == '') {
+                        tanksTable.fnFilter('', 0);
+                    } else {
+                        tanksTable.fnFilter("^" + $(this).val() + "$", 0, true, false);
+                    }
+                });
+                $('.customFilters select:nth-child(3)').change(function () {
+                    if ($(this).val() == '') {
+                        tanksTable.fnFilter('', 3);
+                    } else {
+                        tanksTable.fnFilter("^" + $(this).val() + "$", 3, true, false);
+                    }
+                });
             });
         };
         return MasterViewModel;
